922cd9a4c1157ae8cb0f1e046c70586b01ab074d
Marshall Farrier <info@marshallfarrier.com>
2012-12-16 17:12:59 -0800

    DirectedEdgeDataGraph

---
+++ src/com/codemelon/graph/graph/DirectedEdgeDataGraph.java
@@ -0,0 +1,53 @@
+package com.codemelon.graph.graph;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import com.codemelon.graph.edge.EdgeDataFactory;
+import com.codemelon.graph.vertex.DirectedEdgeDataVertex;
+
+/**
+ * Directed graph that supports edge data. The type parameter T is the type of the edge
+ * data object, and U is an object containing a factory method for creating objects
+ * of type T.
+ * @author Marshall Farrier
+ * @my.created Dec 16, 2012
+ * @my.edited Dec 16, 2012
+ */
+public class DirectedEdgeDataGraph<T, U extends EdgeDataFactory<T>> 
+		extends AbstractGraph<DirectedEdgeDataVertex<T, U>> {
+	/**
+	 * Initialize graph to a default initial capacity of 16 vertices
+	 */
+	public DirectedEdgeDataGraph() {
+		super();
+	}
+	/**
+	 * Initialize graph to have capacity for the given number of vertices
+	 * @param initialCapacity expected number of vertices
+	 */
+	public DirectedEdgeDataGraph(int initialCapacity) {
+		super(initialCapacity);
+	}
+	/**
+	 * Use the vertices in a collection as the initial vertices in the graph
+	 * @param vertices collection of vertices that will make up the initial vertices
+	 * of the graph
+	 */
+	public DirectedEdgeDataGraph(Collection<DirectedEdgeDataVertex<T, U>> vertices) {
+		super(vertices);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.codemelon.graph.graph.AbstractGraph#edgeCount()
+	 */
+	@Override
+	public int edgeCount() {
+		int result = 0;
+		Iterator<DirectedEdgeDataVertex<T, U>> it = super.vertexIterator();
+		while (it.hasNext()) {
+			result += it.next().adjacencyCount();
+		}
+		return result;
+	}
+}
