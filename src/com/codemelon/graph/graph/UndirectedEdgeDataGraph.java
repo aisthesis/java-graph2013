bfbb0a2eecd6645fe4041743ed9b24f0755c766d
Marshall Farrier <info@marshallfarrier.com>
2012-12-16 19:44:59 -0800

    UndirectedEdgeDataGraph

--- src/com/codemelon/graph/graph/UndirectedEdgeDataGraph.java
+++ src/com/codemelon/graph/graph/UndirectedEdgeDataGraph.java
@@ -3,25 +3,30 @@ package com.codemelon.graph.graph;
 import java.util.Collection;
 import java.util.Iterator;
 
-import com.codemelon.graph.vertex.UndirectedSimpleVertex;
+import com.codemelon.graph.edge.EdgeDataFactory;
+import com.codemelon.graph.vertex.UndirectedEdgeDataVertex;
 
 /**
+ * Undirected graph that supports edge data. The type parameter T is the type of the edge
+ * data object, and U is an object containing a factory method for creating objects
+ * of type T.
  * @author Marshall Farrier
- * @my.created Dec 14, 2012
- * @my.edited Dec 14, 2012
+ * @my.created Dec 16, 2012
+ * @my.edited Dec 16, 2012
  */
-public class UndirectedSimpleGraph extends AbstractGraph<UndirectedSimpleVertex> {
+public class UndirectedEdgeDataGraph<T, U extends EdgeDataFactory<T>> 
+		extends AbstractGraph<UndirectedEdgeDataVertex<T, U>> {
 	/**
 	 * Initialize graph to a default initial capacity of 16 vertices
 	 */
-	public UndirectedSimpleGraph() {
+	public UndirectedEdgeDataGraph() {
 		super();
 	}
 	/**
 	 * Initialize graph to have capacity for the given number of vertices
 	 * @param initialCapacity expected number of vertices
 	 */
-	public UndirectedSimpleGraph(int initialCapacity) {
+	public UndirectedEdgeDataGraph(int initialCapacity) {
 		super(initialCapacity);
 	}
 	/**
@@ -29,7 +34,7 @@ public class UndirectedSimpleGraph extends AbstractGraph<UndirectedSimpleVertex>
 	 * @param vertices collection of vertices that will make up the initial vertices
 	 * of the graph
 	 */
-	public UndirectedSimpleGraph(Collection<UndirectedSimpleVertex> vertices) {
+	public UndirectedEdgeDataGraph(Collection<UndirectedEdgeDataVertex<T, U>> vertices) {
 		super(vertices);
 	}
 
@@ -39,7 +44,7 @@ public class UndirectedSimpleGraph extends AbstractGraph<UndirectedSimpleVertex>
 	@Override
 	public final int edgeCount() {
 		int result = 0;
-		Iterator<UndirectedSimpleVertex> it = super.vertexIterator();
+		Iterator<UndirectedEdgeDataVertex<T, U>> it = super.vertexIterator();
 		while (it.hasNext()) {
 			result += it.next().adjacencyCount();
 		}
