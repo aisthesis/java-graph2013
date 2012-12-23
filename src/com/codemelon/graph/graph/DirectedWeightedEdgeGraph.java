d5ae16b31fc91deaa6e8eb460f59dad894b491f3
Marshall Farrier <info@marshallfarrier.com>
2012-12-23 14:17:06 -0600

    Classes for determining edge weight equality

---
+++ src/com/codemelon/graph/graph/DirectedWeightedEdgeGraph.java
@@ -0,0 +1,97 @@
+package com.codemelon.graph.graph;
+
+import java.util.Collection;
+
+import com.codemelon.graph.edge.EdgeDataFactory;
+import com.codemelon.graph.edge.EdgeWeightData;
+import com.codemelon.graph.vertex.DirectedEdgeDataVertex;
+import com.codemelon.graph.vertex.EdgeWeightVertex;
+
+/**
+ * @author Marshall Farrier
+ * @my.created Dec 23, 2012
+ * @my.edited Dec 23, 2012
+ */
+public class DirectedWeightedEdgeGraph<E extends EdgeWeightData, U extends EdgeDataFactory<E>, 
+		V extends DirectedEdgeDataVertex<E, U> & EdgeWeightVertex<E>> extends DirectedEdgeDataGraph<E, U, V>
+		implements WeightedEdgeGraph {
+	private double weightEpsilon;
+	
+	/**
+	 * Initialize an empty graph using the default values for weightEpsilon and initial capacity. 
+	 */
+	public DirectedWeightedEdgeGraph() {
+		this(WeightedEdgeGraph.DEFAULT_WEIGHT_EPSILON);
+	}
+
+	/**
+	 * Initialize an empty graph using the given value for weightEpsilon and default initial capacity. 
+	 * The input value must be greater than 0.
+	 * @param weightEpsilon weight epsilon to be used to determine weight equality
+	 * in this graph
+	 */
+	public DirectedWeightedEdgeGraph(double weightEpsilon) {
+		super();
+		if (weightEpsilon <= 0.0) {
+			throw new IllegalArgumentException("Weight epsilon must be greater than 0!");
+		}
+		this.weightEpsilon = weightEpsilon;
+	}
+	
+	/**
+	 * Use the vertices in a collection as the initial vertices in the graph and
+	 * set weight epsilon to the default value.
+	 * @param vertices collection of vertices that will make up the initial vertices
+	 * of the graph
+	 */
+	public DirectedWeightedEdgeGraph(Collection<V> vertices) {
+		this(vertices, WeightedEdgeGraph.DEFAULT_WEIGHT_EPSILON);
+	}
+
+	/**
+	 * Use the vertices in a collection as the initial vertices in the graph and
+	 * set weight epsilon to the specified value.
+	 * @param vertices collection of vertices that will make up the initial vertices
+	 * of the graph
+	 * @param weightEpsilon weight epsilon to be used to determine weight equality
+	 * in this graph
+	 */
+	public DirectedWeightedEdgeGraph(Collection<V> vertices, double weightEpsilon) {
+		super(vertices);
+		if (weightEpsilon <= 0.0) {
+			throw new IllegalArgumentException("Weight epsilon must be greater than 0!");
+		}
+		this.weightEpsilon = weightEpsilon;
+	}
+
+	/**
+	 * Initialize an empty graph to have capacity for the given number of vertices and
+	 * the default weight epsilon.
+	 * @param initialCapacity expected number of vertices in the graph
+	 */
+	public DirectedWeightedEdgeGraph(int initialCapacity) {
+		this(initialCapacity, WeightedEdgeGraph.DEFAULT_WEIGHT_EPSILON);
+	}
+	/**
+	 * Initialize an empty graph to have capacity for the given number of vertices and
+	 * the specified weight epsilon.
+	 * @param initialCapacity expected number of vertices in the graph
+	 * @param weightEpsilon weight epsilon to be used to determine weight equality
+	 * in this graph
+	 */
+	public DirectedWeightedEdgeGraph(int initialCapacity, double weightEpsilon) {
+		super(initialCapacity);
+		if (weightEpsilon <= 0.0) {
+			throw new IllegalArgumentException("Weight epsilon must be greater than 0!");
+		}
+		this.weightEpsilon = weightEpsilon;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.codemelon.graph.graph.WeightedEdgeGraph#areEqualWeights(double, double)
+	 */
+	@Override
+	public boolean areEqualWeights(double w1, double w2) {
+		return Math.abs(w1 - w2) < weightEpsilon;
+	}
+}
--- src/com/codemelon/graph/graph/KruskalGraph.java
+++ src/com/codemelon/graph/graph/KruskalGraph.java
@@ -7,15 +7,47 @@ import com.codemelon.graph.edge.SpanningTreeEdgeData;
 import com.codemelon.graph.vertex.UndirectedKruskalVertex;
 
 /**
- * Utility class that eliminates one type parameter from UndirectedEdgeDataGraph<E, U, V> by specifying V
- * as an UndirectedDfsVertex<E, U>.
+ * Utility class that eliminates a type parameter from UndirectedWeightedEdgeGraph<E, U, V> by specifying V
+ * as an UndirectedKruskalVertex<E, U>.
  * @author Marshall Farrier
  * @my.created Dec 22, 2012
  * @my.edited Dec 22, 2012
  */
 public class KruskalGraph<E extends SpanningTreeEdgeData, U extends EdgeDataFactory<E>> 
-		extends UndirectedEdgeDataGraph<E, U, UndirectedKruskalVertex<E, U>> {
+		extends UndirectedWeightedEdgeGraph<E, U, UndirectedKruskalVertex<E, U>> {
 
+	/**
+	 * Use the vertices in a collection as the initial vertices in the graph and
+	 * set weight epsilon to the specified value.
+	 * @param vertices collection of vertices that will make up the initial vertices
+	 * of the graph
+	 * @param weightEpsilon weight epsilon to be used to determine weight equality
+	 * in this graph
+	 */
+	public KruskalGraph(Collection<UndirectedKruskalVertex<E, U>> vertices,
+			double weightEpsilon) {
+		super(vertices, weightEpsilon);
+	}
+
+	/**
+	 * Initialize an empty graph using the given value for weightEpsilon and default initial capacity. 
+	 * The input value must be greater than 0.
+	 * @param weightEpsilon weight epsilon to be used to determine weight equality
+	 * in this graph
+	 */
+	public KruskalGraph(double weightEpsilon) {
+		super(weightEpsilon);
+	}
+	/**
+	 * Initialize an empty graph to have capacity for the given number of vertices and
+	 * the specified weight epsilon.
+	 * @param initialCapacity expected number of vertices in the graph
+	 * @param weightEpsilon weight epsilon to be used to determine weight equality
+	 * in this graph
+	 */
+	public KruskalGraph(int initialCapacity, double weightEpsilon) {
+		super(initialCapacity, weightEpsilon);
+	}
 	/**
 	 * Initialize graph to a default initial capacity of 16 vertices
 	 */
