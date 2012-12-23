9b9b17900a064909a36623244ad0db68b88fc723
Marshall Farrier <info@marshallfarrier.com>
2012-12-22 19:27:46 -0800

    Unit tests for UndirectedWeightedEdge

---
+++ tst/com/codemelon/graph/edge/UndirectedWeightedEdgeTest.java
@@ -0,0 +1,70 @@
+package com.codemelon.graph.edge;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Marshall Farrier
+ * @my.created Dec 22, 2012
+ * @my.edited Dec 22, 2012
+ */
+public class UndirectedWeightedEdgeTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.codemelon.graph.edge.UndirectedWeightedEdge#from()}.
+	 */
+	@Test
+	public void testFrom() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.codemelon.graph.edge.UndirectedWeightedEdge#to()}.
+	 */
+	@Test
+	public void testTo() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.codemelon.graph.edge.UndirectedWeightedEdge#weight()}.
+	 */
+	@Test
+	public void testWeight() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.codemelon.graph.edge.UndirectedWeightedEdge#getGraph()}.
+	 */
+	@Test
+	public void testGetGraph() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.codemelon.graph.edge.UndirectedWeightedEdge#equals(java.lang.Object)}.
+	 */
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+}
